e506473c238683ab7a88bf79ba8be98e985f9860
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-08-05 16:42:41 -0300

    create methods in Fraction.hs

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -2,30 +2,28 @@ module Fraction (Fraction, add, sub, mul, divide, hcf) where
 
 type Fraction = (Int, Int)
 
--- Implement the `add` Function
+-- Helper function to simplify fractions
+simplify :: Fraction -> Fraction
+simplify (num, den) = (num `div` h, den `div` h)
+  where h = hcf (abs num) (abs den)
 
+-- Implement the `add` Function
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add (n1, d1) (n2, d2) = (n1 * d2 + n2 * d1, d1 * d2)
 
 -- Implement the `sub` Function
-
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (n1, d1) (n2, d2) = (n1 * d2 - n2 * d1, d1 * d2)
 
 -- Implement the `mul` Function
-
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (n1, d1) (n2, d2) = (n1 * n2, d1 * d2)
 
 -- Implement the `divide` Function
-
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (n1, d1) (n2, d2) = (n1 * d2, d1 * n2)
 
 -- Implement the `hcf` Function
-
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
-
-
-    
+hcf a 0 = a
+hcf a b = hcf b (a `mod` b)
