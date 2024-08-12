1b7e733fe67c4cde43ae1091f2f72e54e579424c
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-08-12 14:54:59 -0300

    fixed Lists.hs

--- haskell-exercises/lists/src/Lists.hs
+++ haskell-exercises/lists/src/Lists.hs
@@ -2,19 +2,22 @@ module Lists (
     member, union, intersection, difference,
     insert, insertionSort,
     binaryToDecimal, toDecimal, toDec, decimal,
-    firsts, binaryAdd, merge, mergeSort
-) where  -- Fix the misplaced closing parenthesis here
+    firsts, binaryAdd
+) where
 
 import Data.Char (digitToInt, intToDigit)
 
 -- List as Sets
 
 member :: Int -> [Int] -> Bool
+member :: Int -> [Int] -> Bool
 member _ []      = False
 member e (x:xs)  = e == x || member e xs
 
 union :: [Int] -> [Int] -> [Int]
+union :: [Int] -> [Int] -> [Int]
 union [] ys     = ys
+union (x:xs) ys
 union (x:xs) ys
   | member x ys = union xs ys
   | otherwise   = x : union xs ys
@@ -24,8 +27,13 @@ intersection [] _ = []
 intersection (x:xs) ys
   | member x ys = x : intersection xs ys
   | otherwise   = intersection xs ys
+intersection :: [Int] -> [Int] -> [Int]
+intersection [] _ = []
+intersection (x:xs) ys
+  | member x ys = x : intersection xs ys
+  | otherwise   = intersection xs ys
 
-difference :: [Int] -> [Int] -> [Int]  -- Example: difference [1, 2, 3, 4] [3, 4, 5, 6] = [1, 2]
+difference :: [Int] -> [Int] -> [Int]
 difference [] _ = []
 difference (x:xs) ys
   | member x ys = difference xs ys
@@ -38,14 +46,20 @@ insert e [] = [e]
 insert e (x:xs)
   | e <= x    = e : x : xs
   | otherwise = x : insert e xs
+insert :: Int -> [Int] -> [Int]
+insert e [] = [e]
+insert e (x:xs)
+  | e <= x    = e : x : xs
+  | otherwise = x : insert e xs
 
 insertionSort :: [Int] -> [Int]
-insertionSort [] = []
-insertionSort (x:xs) = insert x (insertionSort xs)
+insertionSort = foldr insert []
 
 -- Numeral Systems
 
 binaryToDecimal :: [Int] -> Int
+binaryToDecimal = foldl (\acc x -> acc * 2 + x) 0
+
 binaryToDecimal = foldl (\acc x -> acc * 2 + x) 0
 
 toDecimal :: Int -> [Int] -> Int
@@ -78,19 +92,3 @@ addBinary (x:xs) (y:ys) carry
   | sum == 3  = '1' : addBinary xs ys '1'
   | otherwise = intToDigit sum : addBinary xs ys '0'
   where sum = digitToInt x + digitToInt y + digitToInt carry
-
--- Merge Sort
-
-merge :: [Int] -> [Int] -> [Int]
-merge [] ys = ys
-merge xs [] = xs
-merge (x:xs) (y:ys)
-  | x <= y    = x : merge xs (y:ys)
-  | otherwise = y : merge (x:xs) ys
-
-mergeSort :: [Int] -> [Int]
-mergeSort [] = []
-mergeSort [x] = [x]
-mergeSort xs = merge (mergeSort left) (mergeSort right)
-  where
-    (left, right) = splitAt (length xs `div` 2) xs
